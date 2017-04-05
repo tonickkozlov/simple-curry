e42b4c6b85acf7537cfdc2dd2b880a23a204def8
Anton Kozlov <tonickkozlov@me.com>
2017-04-05 16:20:15 +0300

    [feat] Simple curry implementation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-'use strict'
+module.exports = function(curryTarget) {
+    'use strict'
+
+    if (curryTarget.length === 1) {
+        return curryTarget
+    }
+
+    return function curried(...args) {
+        return args.length < curryTarget.length ?
+            curried.bind(this, ...args) :
+            curryTarget.apply(this, args)
+    }
+} 
+
--- index.spec.js
+++ index.spec.js
@@ -2,4 +2,53 @@ const test = require('tape')
 
 const curry = require('./index.js')
 
+const testFn = function(foo, bar, baz) {
+    return foo + bar + baz
+}
 
+test('no curry', t => {
+    t.equal(testFn(1, 2, 3), 6)
+    t.end()
+})
+
+test('curry', t => {
+    t.equal(curry(testFn)(1)(2)(3), 6)
+    t.end()
+})
+
+test('one arg', t => {
+    const toCurry = a => a+1
+    t.equal(curry(toCurry)(1), 2)
+    t.end()
+})
+
+test('overcurry', t => {
+    t.equal(curry(testFn)(1)(2)(3, 4), 6)
+    t.end()
+})
+
+test('all params', t => {
+    t.equal(curry(testFn)(1, 2, 3), 6)
+    t.end()
+})
+
+test('curry 2', t => {
+    t.equal(curry(testFn)(1, 2)(3), 6)
+    t.end()
+})
+
+test('bind', t => {
+    const bound = curry(testFn).bind(null, 1, 2)
+    t.equal(bound(3), 6)
+    t.end()
+})
+
+test('preserve context', t => {
+    const fn = function(a, b, c) {
+        return this.foo + a + b + c
+    }
+    t.equal(fn.call({ foo: 10 }, 1, 2, 3), 16)
+    const curried = curry(fn).call({ foo: 10 }, 1)
+    t.equal(curried(2)(3), 16)
+    t.end()
+})
